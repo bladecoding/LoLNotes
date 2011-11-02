136db27b5799b5b14884e8416b04b7fcb399fa1f
high <pooop828@hotmail.com>
2011-11-02 14:22:29 -0400

    Added EditPlayerForm for editing player's note/color.

---
+++ LoLNotes/Gui/EditPlayerForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace LoLNotes.Gui
+{
+    partial class EditPlayerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NoteText = new System.Windows.Forms.TextBox();
+            this.ColorBox = new System.Windows.Forms.ComboBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NoteText
+            // 
+            this.NoteText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NoteText.Location = new System.Drawing.Point(12, 12);
+            this.NoteText.Name = "NoteText";
+            this.NoteText.Size = new System.Drawing.Size(322, 20);
+            this.NoteText.TabIndex = 0;
+            // 
+            // ColorBox
+            // 
+            this.ColorBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ColorBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.ColorBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ColorBox.FormattingEnabled = true;
+            this.ColorBox.Location = new System.Drawing.Point(12, 38);
+            this.ColorBox.Name = "ColorBox";
+            this.ColorBox.Size = new System.Drawing.Size(241, 21);
+            this.ColorBox.TabIndex = 1;
+            this.ColorBox.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.ColorBox_DrawItem);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveButton.Location = new System.Drawing.Point(259, 36);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 2;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // EditPlayerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 66);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.ColorBox);
+            this.Controls.Add(this.NoteText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "EditPlayerForm";
+            this.Text = "Edit ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveButton;
+        public System.Windows.Forms.TextBox NoteText;
+        public System.Windows.Forms.ComboBox ColorBox;
+    }
+}
---
+++ LoLNotes/Gui/EditPlayerForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LoLNotes.Storage;
+
+namespace LoLNotes.Gui
+{
+    public partial class EditPlayerForm : Form
+    {
+        public EditPlayerForm()
+        {
+            InitializeComponent();
+
+            foreach (var n in Enum.GetNames(typeof (KnownColor)))
+            {
+                ColorBox.Items.Add(n);
+            }
+
+            ColorBox.SelectedIndex = ColorBox.Items.IndexOf(KnownColor.Green.ToString());
+        }
+
+        public EditPlayerForm(PlayerEntry entry)
+            : this()
+        {
+            if (entry == null)
+                return;
+            NoteText.Text = entry.Note ?? "";
+            ColorBox.SelectedIndex = ColorBox.Items.IndexOf(entry.NoteColor.ToKnownColor().ToString());
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void ColorBox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Rectangle rect = e.Bounds;
+            if (e.Index >= 0)
+            {
+                string n = ((ComboBox)sender).Items[e.Index].ToString();
+                var font = new Font("Arial", 9, FontStyle.Regular);
+                var brush = new SolidBrush(Color.FromKnownColor((KnownColor)Enum.Parse(typeof(KnownColor), n)));
+                g.DrawString(n, font, Brushes.Black, rect.X, rect.Top);
+                g.FillRectangle(brush, rect.X + 110, rect.Y, rect.Width, rect.Height);
+            }
+        }
+    }
+}
