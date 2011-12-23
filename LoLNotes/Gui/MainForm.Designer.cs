d1d0308f6f21cf0e6f6285f5d1741f6b1bacae8d
high <pooop828@hotmail.com>
2011-12-23 10:43:03 -0500

    Release 1.1c

--- LoLNotes/Gui/MainForm.Designer.cs
+++ LoLNotes/Gui/MainForm.Designer.cs
@@ -159,6 +159,7 @@ namespace LoLNotes.Gui
 			this.button1.TabIndex = 3;
 			this.button1.Text = "button1";
 			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Visible = false;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// comboBox1
