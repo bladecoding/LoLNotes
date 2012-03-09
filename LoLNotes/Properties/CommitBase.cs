cc1415a28fcc4e6e317a78239c8c14bdc1f82a57
high <high828@gmail.com>
2012-03-08 20:20:41 -0500

    Added pre-build GitExport

---
+++ LoLNotes/Properties/CommitBase.cs
@@ -0,0 +1,53 @@
+﻿/*
+copyright (C) 2011-2012 by high828@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+using System;
+using System.Linq;
+using System.Reflection;
+
+[AttributeUsage(AttributeTargets.Assembly, Inherited = false, AllowMultiple = true)]
+public class AssemblyCommitAttribute : Attribute
+{
+	public AssemblyCommitAttribute(string raw, string hash, string author, string date, string message, string email)
+	{
+		Raw = raw;
+		Hash = hash;
+		Author = author;
+		Date = date;
+		Message = message;
+		Email = email;
+	}
+
+	public string Raw { get; set; }
+	public string Hash { get; set; }
+	public string Author { get; set; }
+	public string Email { get; set; }
+	public string Date { get; set; }
+	public string Message { get; set; }
+}
+
+public class GitInfo
+{
+	public static AssemblyCommitAttribute Get()
+	{
+		return Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCommitAttribute), false).FirstOrDefault() as AssemblyCommitAttribute;
+	}
+}
---
+++ LoLNotes/Properties/CommitInfo.cs
@@ -0,0 +1,3 @@
+[assembly: AssemblyCommit("commit 3106fff7b6f556c2442ff9b6b1f1a868eae620d0\nAuthor: high <high828@gmail.com>\nDate:   Thu Mar 8 19:22:56 2012 -0500\n\n    Fixed crash when stats are not received.\n", "3106fff7b6f556c2442ff9b6b1f1a868eae620d0", "high", "  Thu Mar 8 19:22:56 2012 -0500", "Fixed crash when stats are not received.", "high828@gmail.com")]
+
+								
