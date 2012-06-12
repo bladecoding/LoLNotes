bd8356c56fe7dd2fd22d5976291de0c39d3f6ed7
high <high828@gmail.com>
2012-06-12 12:22:14 -0400

    Added partial LoginDataPacket

---
+++ LoLNotes/Messages/Account/LoginDataPacket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluorineFx;
+using FluorineFx.AMF3;
+using LoLNotes.Flash;
+using NotMissing;
+
+namespace LoLNotes.Messages.Account
+{
+	[Message(".LoginDataPacket")]
+	public class LoginDataPacket : BaseObject, ICloneable
+	{
+		public LoginDataPacket()
+			: base(null)
+		{
+		}
+
+
+		public LoginDataPacket(ASObject obj)
+			: base(obj)
+		{
+			BaseObject.SetFields(this, obj);
+		}
+
+		[InternalName("allSummonerData")]
+		public AllSummonerData AllSummonerData { get; set; }
+
+
+		public object Clone()
+		{
+			return new LoginDataPacket
+			{
+				AllSummonerData = AllSummonerData.CloneT(),
+			};
+		}
+	}
+}
