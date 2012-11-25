69cf37da7f14c92ee1221cf6cbfe7439af50e8df
Will <will@bladecoding.com>
2012-11-24 20:15:22 -0500

    Release 1.1m

--- LoLNotes/Properties/AssemblyInfo.cs
+++ LoLNotes/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyConfiguration("l")]
+[assembly: AssemblyConfiguration("m")]
 [assembly: AssemblyVersion("1.1")]
 [assembly: AssemblyFileVersion("1.1")]
