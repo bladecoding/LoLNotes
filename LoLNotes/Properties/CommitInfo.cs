c520ccd111c1eaaf2f73ff08d81e6bcc80dd21a2
SebastienGllmt <SebastienGllmt@gmail.com>
2014-11-22 05:10:49 -0800

    NA certificate change in csproj

--- LoLNotes/Properties/CommitInfo.cs
+++ LoLNotes/Properties/CommitInfo.cs
@@ -1,3 +1,3 @@
-[assembly: AssemblyCommit("commit 9471ae5aa9f9fc38b9e4ce46389895230e873771\nAuthor: Will <will@bladecoding.com>\nDate:   Sun Aug 19 01:04:24 2012 -0400\n\n    Changed GameIds/AccountIds/SummonerIds should be Int64s to prevent overflowing.\n", "9471ae5aa9f9fc38b9e4ce46389895230e873771", "Will", "  Sun Aug 19 01:04:24 2012 -0400", "Changed GameIds/AccountIds/SummonerIds should be Int64s to prevent overflowing.", "will@bladecoding.com")]
+[assembly: AssemblyCommit("commit 44f8001af229aff1384ef574e53b93359fd13649\nAuthor: SebastienGllmt <SebastienGllmt@gmail.com>\nDate:   Sat Nov 22 04:46:51 2014 -0800\n\n    new certificate\n", "44f8001af229aff1384ef574e53b93359fd13649", "SebastienGllmt", "  Sat Nov 22 04:46:51 2014 -0800", "new certificate", "SebastienGllmt@gmail.com")]
 
 								
